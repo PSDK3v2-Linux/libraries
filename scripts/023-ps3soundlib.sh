20dfb28187495b673e6a742737d452e829d3c6ee
rhynec <86078431+rhynec@users.noreply.github.com>
2022-01-24 22:51:35 -0600

    Bump ps3soundlib version
    - Revert change which made library installation incompatible with the original SDK release.

--- scripts/023-ps3soundlib.sh
+++ scripts/023-ps3soundlib.sh
@@ -3,7 +3,7 @@
 # PS3 SOUNDLIB Credits: Hermes, HACKERCHANNEL, Xiph.Org, mpg123 project and Wargio/deroad
 
 ## Download the source code.
-wget --no-check-certificate https://github.com/rhynec/ps3soundlib/archive/a42a75df53f4d6d1dbf20cd7694ef3634e4e96e8.tar.gz -O ps3soundlib.tar.gz 
+wget --no-check-certificate https://github.com/rhynec/ps3soundlib/archive/ff9d045c3ffa94fd336e992496e1eb55d9f1fd17.tar.gz -O ps3soundlib.tar.gz 
 
 ## Unpack the source code.
 rm -Rf ps3soundlib && mkdir ps3soundlib && tar --strip-components=1 --directory=ps3soundlib -xvzf ps3soundlib.tar.gz && cd ps3soundlib
